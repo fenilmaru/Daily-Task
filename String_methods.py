b0c5d05f1683d7246ffa97a1c8ddb32600d9d2b7
Fenil Maru <fenilh1323@gmail.com>
2026-01-28 18:16:15 +0530

    add file String_methods

---
+++ String_methods.py
@@ -0,0 +1,646 @@
+# Tesing Funtions
+print("String Methods :\n")
+
+# . index example1
+name = "fenil maru"
+value = name.index("a")
+print("index ex1 value is: ",value)
+print(" ")
+
+# . index example2
+s_name = "Stu_names = Harshil, Fenil"
+sname_val= s_name.index("i")
+print("index ex2 value is: ",sname_val)
+print(" ")
+
+# . index example3
+con_list = "Contact list 9409472381, 9512074775"
+con_val = con_list.index("7")
+print("index ex3 value is: ", con_val)
+print(" ")
+
+# . index example4
+loc = "Ahmedabad, Amreli"
+loc_val = loc.index("l")
+print("index ex4 value is: ", loc_val)
+print(" ")
+
+# . index example5
+sr_no = "101, 102, 103, 104"
+sr_val = sr_no.index("3")
+print("index ex5 value is: ", sr_no)
+print(" ")
+
+# . isalpha example1
+paragraph = "Mydreamisaidevloper"
+txt = paragraph.isalpha()
+print("isaplha ex1 value is: ", txt)
+print(" ")
+
+# . isalpha example2
+email_valid = "Enter email in letter"
+valid_val = email_valid.isalpha()
+print("isaplha ex2 value is: ", valid_val)
+print(" ")
+
+# . isalpha example3
+stu_table = "It Department Student tables"
+table_val = stu_table.isalpha()
+print("isaplha ex3 value is: ", table_val)
+print(" ")
+
+# . isalpha example4
+location = "Hirawadi, Naroda, Nikol"
+loc_txt = location.isalpha()
+print("isaplha ex4 value is: ", loc_txt)
+print(" ")
+
+# . isalpha example5
+name_txt = "Name only contains in letter."
+name_val = name_txt.isalpha()
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
+# . isAscii example1
+charcter = "fenil 139"
+chr = charcter.isascii()
+print("isascii ex1 value is: ", name_val)
+print(" ")
+
+# . isAscii example2
+math_txt = "sum of chpcha code"
+math_val = math_txt.isascii()
+print("isascii ex2 value is: ", math_val)
+print(" ")
+
+# . isAscii example3
+calculate_txt = "calculate your marks"
+calculate_val = calculate_txt.isascii()
+print("isascii ex3 value is: ", calculate_val)
+print(" ")
+
+# . isAscii example4
+input_char = "enter password tcontains @,#  value"
+input_val = input_char.isascii()
+print("isascii ex4 value is: ", input_val)
+print(" ")
+
+# . isAscii example5
+url_txt = "C:/fenil/139/documant"
+url_val = url_txt.isascii()
+print("isascii ex5 value is: ", url_val)
+print(" ")
+
+# . isalnum example1
+txtnum = "harshil2311"
+tnum = txtnum.isalnum()
+print("isalnum ex1 value is: ", name_val)
+print(" ")
+
+# . isalnum example2
+email_id = "Harshilsorathiya97@gmail.com"
+email_val = email_id.isalnum()
+print("isalnum ex2 value is: ", email_val)
+print(" ")
+
+# . isalnum example3
+ifsc_code = "DJ216454DS354"
+ifsc_val = ifsc_code.isalnum()
+print("isalnum ex3 value is: ", ifsc_val)
+print(" ")
+
+# . isalnum example4
+home_num = "B-23"
+hnum_val = home_num.isalnum()
+print("isalnum ex4 value is: ", hnum_val)
+print(" ")
+
+# . isalnum example5
+model_num = "INFINUM9219"
+model_val = model_num.isalnum()
+print("isalnum ex5 value is: ", model_val)
+print(" ")
+
+# . isdecimal example1
+decnum = "235491105011"
+number = decnum.isdecimal()
+print("isdecimal ex1 value is: ", name_val)
+print(" ")
+
+# . isdecimal example2
+password = "451276"
+pass_val = password.isdecimal()
+print("isdecimal ex2 value is: ", pass_val)
+print(" ")
+
+# . isdecimal example3
+phone_no = "91-34175555"
+no_val = phone_no.isdecimal()
+print("isdecimal ex3 value is: ", no_val)
+print(" ")
+
+# . isdecimal example4
+fual_num = "94.99"
+fual_val = fual_num.isdecimal()
+print("isdecimal ex4 value is: ", fual_val)
+print(" ")
+
+# . isdecimal exampl5
+gps_num = "35434.35435.2542"
+gps_val = gps_num.isdecimal()
+print("isdecimal ex5 value is: ", gps_val)
+print(" ")
+
+# . isdigit example1
+digit = "3434354534"
+dvalue = digit.isdigit()
+print("isdigit ex1 value is: ", name_val)
+print(" ")
+
+# . isdigit example2
+calce = "123456789012"
+calse_val = calce.isdigit()
+print("isdigit ex2 value is: ", calse_val)
+print(" ")
+
+# . isdigit example3
+rasan_num = "45545 53535 87854"
+rasan_val = rasan_num.isdigit()
+print("isdigit ex3 value is: ", rasan_val)
+print(" ")
+
+# . isdigit example4
+Passbook = "552257873435434"
+passbook_val = Passbook.isdigit()
+print("isdigit ex4 value is: ", passbook_val)
+print(" ")
+
+# . isdigit example5
+aadhar_num = "1845 3554 3434"
+adhar_val = aadhar_num.isdigit()
+print("isdigit ex5 value is: ", adhar_val)
+print(" ")
+
+# . isidentifier example1
+identy = "Ba23484"
+ivalue = identy.isidentifier()
+print("isidentifier ex1 value is: ", name_val)
+print(" ")
+
+# . isidentifier example2
+var_name = "First_name"
+var_val = var_name.isidentifier()
+print("isidentifier ex2 value is: ", var_val)
+print(" ")
+
+# . isidentifier example3
+generate_num = "G-113614"
+generate_val = generate_num.isidentifier()
+print("isidentifier ex3 value is: ", generate_val)
+print(" ")
+
+# . isidentifier example4
+barcode = "ADGARS44212425"
+bar_val = barcode.isidentifier()
+print("isidentifier ex4 value is: ", bar_val)
+print(" ")
+
+# . isidentifier example5
+Natinal_id = "GJ"
+na_val = Natinal_id.isidentifier()
+print("isidentifier ex5 value is: ", na_val)
+print(" ")
+
+# . islower example1
+lowr = "Django is framework"
+l_val = lowr.islower()
+print("islower ex1 value is: ", name_val)
+print(" ")
+
+# . islower example2
+name_plate = "f-14 Maru Family"
+plate_val = name_plate.islower()
+print("islower ex2 value is: ", plate_val)
+print(" ")
+
+# . islower example3
+id_card = "FEnil h. Maru"
+id_val = id_card.islower()
+print("islower ex3 value is: ", id_val)
+print(" ")
+
+# . isnumeric example1
+numc = "32135"
+n_val = numc.isnumeric()
+print("isnumeric ex1 value is: ", name_val)
+print(" ")
+
+# . isnumeric example2
+numc = "32135"
+n_val = numc.isnumeric()
+print("isnumeric ex2 value is: ", name_val)
+print(" ")
+
+# . isnumeric example3
+pincode = "382345"
+pin_val = pincode.isnumeric()
+print("isnumeric ex3 value is: ", pin_val)
+print(" ")
+
+# . isnumeric example4
+seat_num = "10"
+s_val = seat_num.isnumeric()
+print("isnumeric ex4 value is: ", s_val)
+print(" ")
+
+# . isnumeric example5
+otp = "32135"
+otp_val = numc.isnumeric()
+print("isnumeric ex5 value is: ", otp_val)
+print(" ")
+
+# . isprintable example1
+prnt = "Scottenex ITBase"
+pr_val = prnt.isprintable()
+print("isprintable ex1 value is: ", name_val)
+print(" ")
+
+# . isprintable example2
+new_line = "R.b.institute"
+new_val = new_line.isprintable()
+print("isprintable ex2 value is: ", new_val)
+print(" ")
+
+# . isprintable example3
+control_code = "Scottenex_@2025"
+control_val = control_code.isprintable()
+print("isprintable ex3 value is: ", control_val)
+print(" ")
+
+# . isprintable example4
+fix_proble = "Scottenex..ITBase.in"
+prob_val = fix_proble.isprintable()
+print("isprintable ex4 value is: ", prob_val)
+print(" ")
+
+# . isspace example1
+spce = "9512074775"
+sp_val = spce.isspace()
+print("isspace ex1 value is: ", name_val)
+print(" ")
+
+# . isspace example2
+spce = "9512074775"
+sp_val = spce.isspace()
+print("isspace ex2 value is: ", name_val)
+print(" ")
+
+# . isspace example3
+email = "fenil maru1394@gmail.com"
+em_val = email.isspace()
+print("isspace ex3 value is: ", em_val)
+print(" ")
+
+# . istitle example1
+titl = "R.B.Institute"
+ti_val = titl.title()
+print("istitle ex1 value is: ", name_val)
+print(" ")
+
+# . istitle example2
+School_lc = "Vidhaynagar High School"
+slc_val = School_lc.title()
+print("istitle ex2 value is: ", slc_val)
+print(" ")
+
+# . istitle example3
+team_name = "Gujarat Tita;lns"
+t_val = team_name.title()
+print("istitle ex3 value is: ", t_val)
+print(" ")
+
+# . istitle example4
+headline = "R.B.Institute approved by gtu"
+head_val = headline.title()
+print("istitle ex4 value is: ", head_val)
+print(" ")
+
+# . isupper example1
+upr = "w3 school"
+up_val = upr.upper()
+print("isupper ex1 value is: ", name_val)
+print(" ")
+
+# . isupper example2
+ATM_card = "FENIL HASMUKHBHAI MARU"
+Card_val = ATM_card.upper()
+print("isupper ex2 value is: ", Card_val)
+print(" ")
+
+# . isupper example3
+Bank = "SBI OF INDIA"
+bank_val = Bank.upper()
+print("isupper ex3 value is: ", bank_val)
+print(" ")
+
+# casefold example1
+cse_fold = "itbase.in education hub"
+case_val = cse_fold.casefold()
+print("casefold ex1 value is: ", name_val)
+print(" ")
+
+# casefold example2
+cse_name = "Harshil sorathiya"
+cname_val = cse_name.casefold()
+print("casefold ex2 value is: ", cname_val)
+print(" ")
+
+# casefold example3
+cse_value = "education hub"
+cse_val = cse_value.casefold()
+print("casefold ex3 value is: ", cse_val)
+print(" ")
+
+# casefold example4
+cse_input = "User name will be required"
+cinput_val = cse_input.casefold()
+print("casefold ex4 value is: ", cinput_val)
+print(" ")
+
+
+# capitaize example1
+capital = "education in Ai"
+cap_val = capital.capitalize()
+print("capitaize ex1 value is: ", name_val)
+print(" ")
+
+# capitaize example2
+par_txt = "fenil is ai devloper \n harshil sorathiys is designer."
+par_val = par_txt.capitalize()
+print("capitaize ex2 value is: ", par_val)
+print(" ")
+
+# capitaize example3
+number_plt = "gj 01 hk 9219"
+plt_val = number_plt.capitalize()
+print("capitaize ex3 value is: ", plt_val)
+print(" ")
+
+# capitaize example4
+room_name = "r-543"
+rname_val = room_name.capitalize()
+print("capitaize ex4 value is: ", rname_val)
+print(" ")
+
+# center example1
+centr = "itbase.in CodeBase.in"
+ctr_val = centr.casefold()
+print("center ex1 value is: ", name_val)
+print(" ")
+
+# center example2
+txt_name = "gujarat Technology univercity"
+tname_val = txt_name.casefold()
+print("center ex2 value is: ", tname_val)
+print(" ")
+
+# center example3
+name_city = "Ahmedabad"
+city_val = name_city.casefold()
+print("center ex3 value is: ", city_val)
+print(" ")
+
+# center example4
+category = "Fruits, Vegetables, Fast foods"
+cat_val = category.casefold()
+print("center ex4 value is: ", cat_val)
+print(" ")
+
+# endswith example1
+ends = "Ahmedabad"
+ends_val = ends.endswith("d")
+print("endswith ex1 value is: ", name_val)
+print(" ")
+
+# endswith example2
+address = "Hirawadi"
+add_val = address.endswith("d")
+print("endswith ex2 value is: ", add_val)
+print(" ")
+
+# endswith example3
+ed_name = "Viral_panchal"
+ed_val = ed_name.endswith("l")
+print("endswith ex3 value is: ", ed_val)
+print(" ")
+
+# endswith example4
+card = "atm card."
+card_val = ends.endswith(".")
+print("endswith ex4 value is: ", card_val)
+print(" ")
+
+# count example1
+cont = "Harshil, Fenil, Rishika"
+cont_val = cont.count("a")
+print("count ex1 value is: ", name_val)
+print(" ")
+
+# count example2
+num_count = "9512074775, 9409472381"
+ncount_val = num_count.count("7")
+print("count ex2 value is: ", ncount_val)
+print(" ")
+
+# count example3
+bulding = "Suraj park, Jahnki Hights"
+buld_val = bulding.count("n")
+print("count ex3 value is: ", buld_val)
+print(" ")
+
+# count example4
+table_name = "Student Records"
+tblname_val = table_name.count("o")
+print("count ex4 value is: ", tblname_val)
+print(" ")
+
+# encode example1
+ecode = "9409551653134384"
+ecode_val = ecode.encode()
+print("encode ex1 value is: ", name_val)
+print(" ")
+
+# encode example2
+contact_numb = "99586263094"
+cnum_val = contact_numb.encode()
+print("encode ex2 value is: ", cnum_val)
+print(" ")
+
+# encode example3
+img_code = "AD51653134384.jpg"
+icode_val = img_code.encode()
+print("encode ex3 value is: ", icode_val)
+print(" ")
+
+# find example1
+find_indx = "35, suraj park, hirawadi"
+find_val = find_indx.find("park")
+print("find ex1 value is: ", name_val)
+print(" ")
+
+# find example2
+loc_indx = "B-35, suraj park, hirawadi"
+locnew_val = loc_indx.find("k")
+print("find ex2 value is: ", locnew_val)
+print(" ")
+
+# find example3
+numb_indx = "B23, Shantinagar Society"
+numb_val = numb_indx.find("S")
+print("find ex3 value is: ", numb_val)
+print(" ")
+
+# find example4
+name_indx = "suraj park"
+nameNew_val = name_indx.find("a")
+print("find ex4 value is: ", nameNew_val)
+print(" ")
+
+# join example1
+join_name = "is class topper"
+join_val = "Harshil".join(join_name)
+print("join ex1 value is: ", name_val)
+print(" ")
+
+# join example2
+b_name = "is gtu topper"
+bname_val = "Harshil".join(b_name)
+print("join ex2 value is: ", bname_val)
+print(" ")
+
+# join example3
+sjoin_name = "is goverment school"
+sjoin_val = "KLF".join(sjoin_name)
+print("join ex3 value is: ", sjoin_val)
+print(" ")
+
+# format example
+formt = "{fname} education hub"
+for_val = formt.format(fname = "Codebase.in")
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
+# ljust example
+left_just = "Java,Python,Php"
+ljust_val = left_just.ljust(10)
+print("isaplha ex5 value is: ",ljust_val, "is high-level language")
+print(" ")
+
+# lower example
+low = "python was decalred in 1980s by Guado van Rossum."
+low_val = low.lower()
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
+# lstrp example
+left_strp = " VS Gujarat "
+lstrp_val = left_strp.lstrip()
+print("Golf giants", ljust_val, "Titans")
+print(" ")
+
+# MakeTrans example
+mke_trans = "Google Translateb"
+mtrans_val = str.maketrans("b", ".")
+print("isaplha ex5 value is: ", mke_trans.translate(mtrans_val))
+print(" ")
+
+# partiton example
+part = "My age is 21"
+part_val = part.partition("is")
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
+# replace example
+replce = "My enrollment number 011"
+replce_val = replce.replace("011", "235491105011")
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
+# rfind example
+right_fnd = "Bvoc it, Msc it"
+r_findval = right_fnd.rfind("it")
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
+# rindex example
+rindex = "Her password is ....#@123"
+rindex_val = rindex.rindex("#")
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
+# rjust example
+r_just = "education hub"
+rjust_val = r_just.rjust(20)
+print("isaplha ex5 value is: ", rjust_val, "ITBase.in")
+print(" ")
+
+# rpartiton example
+r_part = "Fenilmaru13@gmail.com"
+rpart_val = r_part.rpartition(".")
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
+# rsrip example
+r_strp = " contact "
+rstrip_val = r_strp.rstrip()
+print("My father", rstrip_val, "number 9879034775")
+print(" ")
+
+# split example
+splittxt = "Ui Ux is designing course"
+split_val = splittxt.split()
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
+# splitlines example
+sp_lines = "itbase.in education hub\nby Scottenex Company"
+spline_val = sp_lines.splitlines()
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
+# startswith example
+start = "itbase.in education hub"
+start_val = start.startswith("e")
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
+# strip example
+striptxt = " Bank "
+strip_val = striptxt.strip()
+print("Today Only 10",splittxt, "accounts opens in.")
+print(" ")
+
+# swapcase example
+swap = "Today New Game Lanunching"
+swap_val = swap.swapcase()
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
+# title example
+titile = "TV9 is famous new in gujarat"
+title_val = titile.title()
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
+# translate example
+trans = {8: 10}
+trans_val = "Fenil Maru"
+print("isaplha ex5 value is: ",trans_val.translate(trans))
+print(" ")
+
+# upper example
+upr = "Tata Constultances Sevices"
+upr_val = upr.upper()
+print("isaplha ex5 value is: ", name_val)
+print(" ")
+
