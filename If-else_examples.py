c173c1df8c7a98e477ae3409bd24f8ad9cf88813
Fenil Maru <fenilh1323@gmail.com>
2026-01-29 20:31:48 +0530

    If-else_examples file

---
+++ If-else_examples.py
@@ -0,0 +1,198 @@
+# example 1 for if else stament
+std1 = 48
+std2 = 38
+std3 = 48
+
+if std1 > std1:
+    print("student1's marks less then student2")
+elif std1 == std3:
+    print("student1 marks is same as student3")
+else:
+    print("stddent2 marks is not match to student 3")
+
+
+# example 2 for if else shopping time
+bag = 20
+
+if bag > 10:
+    print("Only 10 bag are left")
+elif bag == 20:
+    print("bags are avilable on same day")
+else:
+    print("bag is not avilable")
+
+# example 3 for if else stament
+wallet = 10
+
+if wallet > 20:
+    print("only 10 Wallets in stock")
+else:
+    print("out of stock")
+
+# example 4 for if else festival offer
+tickets = 100
+
+if tickets > 150:
+    print("festival tickets are avilable 10 days left")
+elif tickets == 100:
+    print("5 days ticket price is 100 Rupees")
+else:
+    print("ticket will be sell on regular price")
+
+# example 5 for if else exam duration
+exam_time = 120
+
+if exam_time > 100:
+    print("exam time is only 2 hours")
+elif exam_time >= 130:
+    print("only exam time issue in 2 hour")
+else:
+    print("Time out")
+
+# example 6 for if else form duration
+Form_fill = 30
+
+if Form_fill > 20:
+    print("form duration is 30 days")
+elif Form_fill == 30:
+    print("you will fill the form between 30 days")
+else:
+    print("Form date is out of range")
+
+# example 7 for if else adhar update
+adhar_card = 50
+
+if adhar_card > 50:
+    print("you will chage aadhar card in address please pay 50 rupees")
+elif adhar_card <= 100:
+    print("you will chage aadhar card in address, image please pay 100 rupees")
+else:
+    print("Server down, please visit center")
+
+# example 8 for if else stament
+web_search = 5
+
+if web_search < 4:
+    print("common 5 website vists in 2 hours")
+elif web_search == 5:
+    print("visited website on 1 hours")
+else:
+    print("website searching pending ,because network slow")
+
+# example 9 for if else stament
+serial = 20
+
+if serial == 20:
+    print("seral time limit is 20 minits")
+elif serial > 15:
+    print("serial complete in 15 minits")
+else:
+    print("serial time is change")
+
+# example 10 for if else stament
+movie = 180
+
+if movie > 120:
+    print("movie is complete in 20 minits")
+elif movie == 120:
+    print("movie is complete on time")
+else:
+    print("movie is not show today")
+
+# example 11 for if else stament
+g_map = 12
+
+if g_map <= 11:
+    print("google map going to next depo in 12 stops")
+elif g_map == 11:
+    print("google map go to depo in 12 stops")
+else:
+    print("1 stop left")
+
+# example 12 for if else stament
+username = "admin"
+password = "1234"
+
+if username == "admin":
+    print("Login Success")
+elif password == "124":
+    print("Password not match")
+else:
+    print("invalid user")
+
+# example 13 for if else stament
+interview_time = "6 PM"
+
+if interview_time > "3 PM":
+    print("interview time limit is 6 hours")
+elif interview_time == "6 PM":
+    print("interview time completed in 6 hours")
+else:
+    print("interview duration is complete")
+
+# example 14 for if else stament
+time = 10   # 24-hour format
+
+if time == 9:
+    print("9–10 AM : Ai")
+elif time == 10:
+    print("10–11 AM : CS")
+elif time == 11:
+    print("11–11:30 AM : Luch Break")
+elif time == 12:
+    print("11:30–12:30 PM : CC")
+else:
+    print("No Class at this time")
+
+# example 15 for if else stament
+time = 7   # 12-hour format
+
+if time >= 6 and time < 9:
+    print("morning bike riding ")
+elif time >= 9 and time < 17:
+    print("Not recommended")
+else:
+    print("Avoid riding now")
+
+# example 17 for if else stament
+web_open_time = "9 am"
+
+if web_open_time > "10 am":
+    print("goverment wesite open in 10 am")
+elif web_open_time == "9 am":
+    print("goverment wesite opening between 9 am to 6 pm")
+else:
+    print("website open in tomorrow at 9 am")
+
+# example 18 for if else
+Appointment_1 = "10 am"
+Appointment_2 = "5 pm"
+
+if Appointment_1 > Appointment_2:
+    print("Appointment time best time is moring")
+elif Appointment_1 == Appointment_1:
+    print("Appointment as same time")
+else:
+    print("Appointment not vailable")
+
+# example 19 for if else stament
+amount = "Rs.509"
+
+if amount == "Rs.199":
+    print("Recharge: 1.5GB/day for 28 days")
+elif amount == "Rs.509":
+    print("Recharge: 6GB Data for 84 days")
+elif amount == "Rs.1499":
+    print("Recharge: 2.5GB/day for 180 days")
+else:
+    print("Invalid Recharge Amount")
+
+# example 20 for if else stament
+time = "8 AM"
+
+if time <= "7 AM":
+    print("Going fast On time for college")
+elif time == "8 AM":
+    print("Walk On time for college")
+else:
+    print("Running late")
