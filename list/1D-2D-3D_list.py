cf5b49b385755837c835c2189e98cf281b6f4755
Fenil Maru <fenilh1323@gmail.com>
2026-02-03 09:38:40 +0530

    1D-2D-3D_list file

---
+++ list/1D-2D-3D_list.py
@@ -0,0 +1,50 @@
+results = input("Which list do you want?\n1D List\n2D List\n3D List\n")
+
+# 1D LIST
+if(results == "1" or results == "1D"):
+    size = int(input("How many elements do you want? "))
+    one_d = []
+
+    for i in range(size):
+        val = int(input(f"Enter element {i+1}: "))
+        one_d.append(val)
+    print("1D List:", one_d)
+
+
+# 2D LIST
+elif(results == "2" or results == "2D"):
+    rows = int(input("Enter number of rows: "))
+    cols = int(input("Enter number of columns: "))
+    two_d = []
+
+    for i in range(rows):
+        row = []
+        for j in range(cols):
+            val = input(f"Enter element [{i}][{j}]: ")
+            row.append(val)
+        two_d.append(row)
+    print(two_d)
+
+
+# 3D LIST 
+elif(results == "3" or results == "3D"):
+    layers = int(input("Enter number of layers: "))
+    rows = int(input("Enter number of rows: "))
+    cols = int(input("Enter number of columns: "))
+    three_d = []
+
+    for l in range(layers):
+        layer = []
+        print(f"\nLayer {l+1}")
+        for i in range(rows):
+            row = []
+            for j in range(cols):
+                val = input(f"Enter element [{l}][{i}][{j}]: ")
+                row.append(val)
+            layer.append(row)
+        three_d.append(layer)
+    print(three_d)
+
+
+else:
+    print("Invalid choice!")
---
+++ list/2d_userinput.py
@@ -0,0 +1,28 @@
+layer = int(input("enter value: "))
+row = int(input("enter value: "))
+
+data_values = [] 
+
+for i in range(layer):
+    layer_value = []
+    print("layers {i+1}")
+
+    for j in range(row):
+        row_value = []
+
+        val_name = input("enter name: ")
+        val_num = int(input("enter value number: "))
+
+        row_value.append(val_name)
+        row_value.append(val_num)
+
+        layer_value.append(row_value)
+
+    data_values.append(layer_value)
+
+    print(data_values)
+
+
+
+
+
